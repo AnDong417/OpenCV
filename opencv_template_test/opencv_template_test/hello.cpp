50420d4a8819a44507d3fff2265e8e6229fc87e3
anton <andong69@outlook.com>
2023-08-05 20:07:02 +0800

    use iterator to traverse img

--- opencv_template_test/opencv_template_test/hello.cpp
+++ opencv_template_test/opencv_template_test/hello.cpp
@@ -23,15 +23,25 @@ int main()
 {
 	//cv::Mat img = cv::imread("8524487.jpg");
 	cv::Mat img(1920, 1080, CV_8UC3);
-	uchar element = img.elemSize();
-	uint8 size = img.rows * img.cols * element;
 
 	PrintMs();
+#if 0	//use normal
+	uchar element = img.elemSize();
+	uint8 size = img.rows * img.cols * element;
 	for (uint8 i = 0; i < size; i += 3) {
 		img.data[i] = 255;	//B
 		img.data[i + 1] = 100;	//G
 		img.data[i + 2] = 100;	//R
 	}
+#else	//use iterator
+	auto it = img.begin<Vec3b>();
+	auto it_end = img.end<Vec3b>();
+	for (; it != it_end; it++) {
+		(*it).val[0] = 255;	//B
+		(*it).val[1] = 10;	//G
+		(*it).val[2] = 10;	//R
+	}
+#endif
 	double time_ms = PrintMs();
 	cout << "time_ms = " << time_ms << endl;
 
