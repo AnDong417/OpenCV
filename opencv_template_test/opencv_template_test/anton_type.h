2fda36848c67cb178446e6f774c471d1c7467bf7
anton <andong69@outlook.com>
2023-08-05 19:34:52 +0800

    add PrintMs

---
+++ opencv_template_test/opencv_template_test/anton_type.h
@@ -0,0 +1,8 @@
+#ifndef _ANTON_TYPE_H
+#define _ANTON_TYPE_H
+
+typedef unsigned int uint8;
+typedef unsigned char uchar;
+typedef long long int16;
+
+#endif
--- opencv_template_test/opencv_template_test/hello.cpp
+++ opencv_template_test/opencv_template_test/hello.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "anton_type.h"
+using namespace std;
 using namespace cv;
+
+double PrintMs(void)
+{
+	static int16 last = 0;
+	int16 cur = getTickCount();
+
+	if (!last) {
+		last = cur;
+		return 0;
+	}
+	double res = ((double)(cur - last) / getTickFrequency()) * 1000;
+	last = cur;
+
+	return res;
+}
 
 int main()
 {
 	//cv::Mat img = cv::imread("8524487.jpg");
 	cv::Mat img(1920, 1080, CV_8UC3);
+	uchar element = img.elemSize();
+	uint8 size = img.rows * img.cols * element;
 
-	unsigned char element = img.elemSize();
-	unsigned int size = img.rows * img.cols * element;
-	for (int i = 0; i < size; i += 3) {
+	PrintMs();
+	for (uint8 i = 0; i < size; i += 3) {
 		img.data[i] = 255;	//B
 		img.data[i + 1] = 100;	//G
 		img.data[i + 2] = 100;	//R
 	}
+	double time_ms = PrintMs();
+	cout << "time_ms = " << time_ms << endl;
 
 	namedWindow("image");
 	imshow("image", img);
